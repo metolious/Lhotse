30eff9388c03e1b56d0b2688e21f0d03c3358f12
Austin <raekwon0825@gmail.com>
2021-05-29 23:14:31 -0400

    Deleting old src directory, no longer used

--- src/app/classes/SHARED_CLASSES/role.class.ts
+++
@@ -1,6 +0,0 @@
-
-export class Role {
-    public Description:      string     = "";
-    ​​​      public Name:             string     = "";
-    ​​​​​      public RoleId:           number     =  0;S
-}
--- src/upload.php
+++
@@ -1,1 +0,0 @@
- <?php echo '<p>Upload Process</p>'; ?> 
